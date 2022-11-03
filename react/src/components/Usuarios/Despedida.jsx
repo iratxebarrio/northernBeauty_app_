46db3aee9b65b24d0004fe3153934b07109cbb79
IRATXEBARRIO92 <iratxe92@gmail.com>
2022-11-03 09:33:58 +0100

    inicio pagina despedida

--- react/src/components/Usuarios/Despedida.jsx
+++ react/src/components/Usuarios/Despedida.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Despedida = () => {
     return (
-        <div>
-            <h1>Hasta pronto</h1>
+        <div className='despedida_container'>
+            <h1 className='despedida_title'>Hasta pronto</h1>
+            <img className='despedida_img' src="/Images/mano.png" alt="" />
         </div>
     )
 }
