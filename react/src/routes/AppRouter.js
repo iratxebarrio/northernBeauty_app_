f5d7115028db8cffb4dd1c0950ab5fb5770f6d25
IRATXEBARRIO92 <iratxe92@gmail.com>
2022-11-25 10:30:11 +0100

    eliminar archivos innecesarios

--- react/src/routes/AppRouter.js
+++ react/src/routes/AppRouter.js
@@ -3,8 +3,6 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Login from '../components/Usuarios/Login'
 import Register from '../components/Usuarios/Register'
 import Main from '../components/Main'
-import Bienvenida from '../components/Usuarios/Bienvenida';
-import Despedida from '../components/Usuarios/Despedida';
 import PaginaReservar from '../components/Reservas/PaginaReservar';
 import VisualizarReservas from '../components/Reservas/VisualizarReservas'
 import ModificarReserva from '../components/Reservas/ModificarReserva';
@@ -20,8 +18,6 @@ const AppRouter = () => {
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
           <Route path="/registro" element={<Register />} />
-          <Route path="/bienvenida" element={<Bienvenida />} />
-          <Route path="/despedida" element={<Despedida />} />
           <Route path="/reservas/:id" element={<PaginaReservar />} />
           <Route path="/reservas-usuario" element={<VisualizarReservas />} />
           <Route path="/modificar-reserva/:usuario_id/:servicio_id" element={<ModificarReserva />} />
